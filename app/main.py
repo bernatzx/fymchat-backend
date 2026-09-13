f3df8c658e04a217413fb80c86ce37180824692e
bernatzx <andikasukardi1@gmail.com>
2026-09-13 17:55:22 +0700

    add cors origins

--- app/main.py
+++ app/main.py
@@ -2,11 +2,17 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import grammar, translator, paraphrase
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = FastAPI(title="Englify AI API")
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["http://localhost:5173", os.getenv("CORS_ORIGINS")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
